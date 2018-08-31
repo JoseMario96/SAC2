b8b4f2761d73a06b80aed649747b942167eba1fa
JoseMario96 <42684637+JoseMario96@users.noreply.github.com>
2018-08-31 09:57:07 -0600

    Agregando conexion de Mysql

---
+++ SAC/SAC/conexion/conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace SAC.conexion
+{
+    public class conexion
+    {
+        private MySqlConnection con;
+
+        public MySqlConnection Abrir_conexion()
+        {
+            try
+            {
+                con = new MySqlConnection("server=localhost;user id=root;password=Jose.150396;database=bd_sac; SslMode=none");
+                con.Open();
+            }
+            catch (MySqlException)
+            {
+                con = null;
+            }
+            return con;
+        }
+
+        public MySqlConnection Cerrar_conexion()
+        {
+            con.Close();
+            return con;
+        }
+    }
+}
