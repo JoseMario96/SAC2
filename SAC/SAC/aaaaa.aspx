58253957ea9b07f46e0b1e4b1021bf66600713cc
JoseMario96 <42684637+JoseMario96@users.noreply.github.com>
2018-08-31 16:56:04 -0600

    Pantalla emergente

---
+++ SAC/SAC/aaaaa.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="aaaaa.aspx.cs" Inherits="SAC.aaaaa" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+
+    <script language= "javascript">
+    function resize() {
+    resizeTo(800,500);
+    } 
+</script>
+
+</head>
+<body onload="resize();">
+    <form id="form1" runat="server">
+        <asp:Image ID="Image1" runat="server" Height="188px" Width="171px" />
+        <asp:Calendar ID="Calendar1" runat="server" Width="358px"></asp:Calendar>
+    </form>
+</body>
+</html>
---
+++ SAC/SAC/inicio.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SAC
+{
+    public partial class inicio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+
+            
+            string vtn = "window.open('aaaaa.aspx','Dates','scrollbars=yes,resizable=yes','height=100','width=100')";
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "popup", vtn, true);
+        }
+    }
+}
