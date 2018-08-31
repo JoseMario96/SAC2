1b51c732260bb1823d1e003290bb351b67dc1327
dtrejos95 <42871089+dtrejos95@users.noreply.github.com>
2018-08-31 15:25:42 -0600

    Agregar/Actualizar paciente/encargado

--- SAC/SAC/metodos/metodosPaciente.cs
+++ SAC/SAC/metodos/metodosPaciente.cs
@@ -9,9 +9,25 @@ namespace SAC.metodosPaciente
         consulta.consulta consultar = new consulta.consulta();
         conexion.conexion con = new conexion.conexion();
 
-        public void Agregarcita(String cedula, String fecha, String hora, String telefono)
+        public void agregarPaciente(String ced, String nom1, String nom2, String ape1, String ape2, String gen, String tel, String cel, String dir, String enc, String corr, String fechN, String fechI)
         {
-            MySqlDataReader insertar = consultar.ejecutar_consulta("INSERT INTO `bd_sac`.`tbl_cita` (cedulaPaciente, fechaReservaCita, horaCita, telefonoCita) values(' " + cedula + " ', ' " + fecha + " ', ' " + hora + " ', ' " + telefono + " ');", con.abrir_conexion()).ExecuteReader();
+            MySqlDataReader agregar = consultar.ejecutar_consulta("INSERT INTO `bd_sac`.`tbl_paciente` (`cedulaPaciente`, `nombre1Paciente`, `nombre2Paciente`, `apellido1Paciente`, `apellido2Paciente`, `generoPaciente`, `telefonoPaciente`, `celularPaciente`, `direccionPaciente`, `encargadoPaciente`, `correoPaciente`, `fechaNacimientoPaciente`, `fechaInscripcionPaciente`) VALUES(' " + ced + " ', ' " + nom1 + " ', ' " + nom2 + " ', ' " + ape1 + " '' " + ape2 + " ',' " + gen + " ',' " + tel + " ',' " + cel + " ',' " + dir + " ',' " + enc + " ',' " + corr + " ',' " + fechN + " ',' " + fechI + " ');", con.abrir_conexion()).ExecuteReader();
+            con.cerrar_Conexion();
+        }
+        public void actualizarPaciente(String ced, String nom1, String nom2, String ape1, String ape2, String gen, String tel, String cel, String dir, String enc, String corr, String fechN, String fechI)
+        {
+            MySqlDataReader actualizar = consultar.ejecutar_consulta("UPDATE `bd_sac`.`tbl_paciente` SET ``nombre1Paciente`= ' " + nom1 + " ', `nombre2Paciente`= ' " + nom2 + " ', `apellido1Paciente`= ' " + ape1 + " ', `apellido2Paciente`= ' " + ape2 + " ', `generoPaciente`= ' " + gen + " ', `telefonoPaciente`= ' " + tel + " ', `celularPaciente`= ' " + cel + " ', `direccionPaciente`= ' " + dir + " ', `encargadoPaciente`= ' " + enc + " ', `correoPaciente`= ' " + corr + " ', `fechaNacimientoPaciente`= ' " + fechN + " ', `fechaInscripcionPaciente`== ' " + fechI + " ' WHERE `cedulaPaciente`=' " + ced + " ';);", con.abrir_conexion()).ExecuteReader();
+            con.cerrar_Conexion();
+        }
+
+        public void agregarEncargado(String ced, String nom1, String nom2, String ape1, String ape2, String gen, String tel, String cel, String dir, String par, String corr)
+        {
+            MySqlDataReader agregar = consultar.ejecutar_consulta("INSERT INTO `bd_sac`.`tbl_encargado` (`cedulaEncargado`, `nombre1Encargado`, `nombre2Encargado`, `apellido1Encargado`, `apellido2Encargado`, `generoEncargado`, `telefonoEncargado`, `celularEncargado`, `direccionEncargado`, `parentescoEncargado`, `correoEncargado`) VALUES(' " + ced + " ', ' " + nom1 + " ', ' " + nom2 + " ', ' " + ape1 + " '' " + ape2 + " ',' " + gen + " ',' " + tel + " ',' " + cel + " ',' " + dir + " ',' " + par + " ',' " + corr + " ');", con.abrir_conexion()).ExecuteReader();
+            con.cerrar_Conexion();
+        }
+        public void actualizarEncargado(String ced, String nom1, String nom2, String ape1, String ape2, String gen, String tel, String cel, String dir, String par, String corr)
+        {
+            MySqlDataReader agregar = consultar.ejecutar_consulta("UPDATE `bd_sac`.`tbl_encargado` SET `nombre1Encargado`= ' " + nom1 + " ', `nombre2Encargado`= ' " + nom2 + " ', `apellido1Encargado`= ' " + ape1 + " ', `apellido2Encargado`= ' " + ape2 + " ', `generoEncargado`= ' " + gen + " ', `telefonoEncargado`= ' " + tel + " ', `celularEncargado`= ' " + cel + " ', `direccionEncargado`= ' " + dir + " ', `parentescoEncargado`= ' " + par + " ', `correoEncargado`= ' " + corr + " ' WHERE `cedulaEncargado`=' " + ced + " ';);", con.abrir_conexion()).ExecuteReader();
             con.cerrar_Conexion();
         }
     }
