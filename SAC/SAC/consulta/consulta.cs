dd53147e7e0526b3d7d74fc4049c3f9b1b347d19
JoseMario96 <42684637+JoseMario96@users.noreply.github.com>
2018-08-31 10:13:25 -0600

    Clases conexión y consulta en master

---
+++ SAC/SAC/consulta/consulta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace SAC.consulta
+{
+    public class consulta
+    {
+        public consulta()
+        {
+        }
+        public MySqlCommand ejecutar_consulta(String consulta, MySqlConnection con)
+        {
+
+            MySqlCommand comando;
+            try
+            {
+                comando = new MySqlCommand(consulta, con);
+            }
+            catch (MySqlException)
+            {
+                comando = null;
+            }
+            return comando;
+        }
+    }
+}
