01edc41d44f0b88f367fe9c7b181708938556be6
roesm <roesm@DESKTOP-TN1O1LC>
2018-09-18 13:42:18 -0600

    ,

--- SAC/SAC/metodos/metodosExpediente.cs
+++ SAC/SAC/metodos/metodosExpediente.cs
@@ -13,12 +13,7 @@ namespace SAC.metodos
         consulta.consulta consultar = new consulta.consulta();
         conexion.conexion con = new conexion.conexion();
 
-        public void agregarExpediente(String ced, String fechT, String codT, String obs)
-        {
-            MySqlDataReader agregar = consultar.ejecutar_consulta("INSERT INTO `bd_sac`.`tbl_paciente` (`cedulaPaciente`, `nombre1Paciente`, `nombre2Paciente`, `apellido1Paciente`, `apellido2Paciente`, `generoPaciente`, `telefonoPaciente`, `celularPaciente`, `direccionPaciente`, `encargadoPaciente`, `correoPaciente`, `fechaNacimientoPaciente`, `fechaInscripcionPaciente`) VALUES(' " + ced + " ', ' " + fechT + " ', ' " + codT + " ', ' " + obs + " ');", con.abrir_conexion()).ExecuteReader();
-            con.cerrar_Conexion();
-        }
-
+      
         public DataTable BuscarExpediente()
         {
             string consulta = "select tbl_expediente.codigoExpediente, tbl_expediente.cedulaPaciente, tbl_paciente.nombre1Paciente, tbl_paciente.apellido1Paciente, tbl_paciente.apellido2Paciente from tbl_expediente, tbl_paciente where tbl_paciente.cedulaPaciente = tbl_expediente.cedulaPaciente;";
