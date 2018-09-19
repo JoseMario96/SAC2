bfd0f9a83e89f361d74784db0f6eff67faf0b3bf
dtrejos95 <42871089+dtrejos95@users.noreply.github.com>
2018-09-19 16:56:51 -0600

    Canvas

--- SAC/SAC/formularios/Prueba.aspx.cs
+++ SAC/SAC/formularios/Prueba.aspx.cs
@@ -18,27 +18,6 @@ namespace SAC.formularios
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            if (!this.IsPostBack)
-            {
-                GridView1.DataSource = objeto.Paciente();
-                GridView1.DataBind();
-
-            }
-        }
-
-        protected void OnDataBound(object sender, EventArgs e)
-        {
-            GridViewRow row = new GridViewRow(0, 0, DataControlRowType.Header, DataControlRowState.Normal);
-            for (int i = 0; i < GridView1.Columns.Count; i++)
-            {
-                TableHeaderCell cell = new TableHeaderCell();
-                TextBox txtSearch = new TextBox();
-                txtSearch.Attributes["placeholder"] = GridView1.Columns[i].HeaderText;
-                txtSearch.CssClass = "search_textbox";
-                cell.Controls.Add(txtSearch);
-                row.Controls.Add(cell);
-            }
-            GridView1.HeaderRow.Parent.Controls.AddAt(1, row);
         }
     }
 }
