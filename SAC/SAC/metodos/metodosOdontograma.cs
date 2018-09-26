dfc4a60c1ccca64ab71a7551128c90d069c11739
dtrejos95 <42871089+dtrejos95@users.noreply.github.com>
2018-09-26 15:53:20 -0600

    prueba

---
+++ SAC/SAC/metodos/metodosOdontograma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAC.metodos
+{
+    public class metodosOdontograma
+    {
+        consulta.consulta consultar = new consulta.consulta();
+        conexion.conexion con = new conexion.conexion();
+
+        public void agregarOdontograma(String col, String die, String secc, String cont)
+        {
+            consultar.ejecutar_consulta("INSERT INTO `bd_sac`.`tbl_odontograma` (`colorOdontograma`, `dienteOdontograma`, `seccionOdontograma`, `contextoOdontograma`) VALUES ('" + col + "', '" + die + "', '" + secc + "', '" + cont + "');", con.abrir_conexion()).ExecuteNonQuery();
+            con.cerrar_Conexion();
+        }
+    }
+}
