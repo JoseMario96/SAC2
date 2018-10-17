a4e2102909ddebc1a14e8ec938f7c40a28206d2f
roesm <roesm@DESKTOP-TN1O1LC>
2018-10-17 14:46:13 -0600

    Cambios odontograma

--- SAC/SAC/metodos/metodosOdontograma.cs
+++ SAC/SAC/metodos/metodosOdontograma.cs
@@ -46,5 +46,26 @@ namespace SAC.metodos
             return estado;
            
         }
+
+        public void agregarOdontograma2(String marc)
+        {
+
+            consultar.ejecutar_consulta("INSERT INTO `bd_sac`.`tbl_odontograma` (`marcaOdontograma`) VALUES ('" + marc + "');", con.abrir_conexion()).ExecuteNonQuery();
+            con.cerrar_Conexion();
+        }
+        public String[] buscarOdontograma(String odo)
+        {
+            string[] stringArray1 = new string[4];
+            MySqlDataReader actualizar = consultar.ejecutar_consulta("SELECT colorOdontograma, dienteOdontograma,seccionOdontograma,marcaOdontograma FROM bd_sac.tbl_odontograma where codigoOdontograma = '" + odo + "'; ", con.abrir_conexion()).ExecuteReader();
+            while (actualizar.Read())
+            {
+                //stringArray1[0] = actualizar.GetString(0);
+                //stringArray1[1] = actualizar.GetString(1);
+                //stringArray1[2] = actualizar.GetString(2);
+                stringArray1[3] = actualizar.GetString(3);
+            }
+            con.cerrar_Conexion();
+            return stringArray1;
+        }
     }
 }
