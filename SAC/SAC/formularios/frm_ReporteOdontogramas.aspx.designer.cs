8854399b5ab4c128c69a37f8c34b4230e52d8452
roesm <roesm@DESKTOP-TN1O1LC>
2018-11-16 20:21:54 -0600

    Reporte odontograma

--- SAC/SAC/formularios/frm_ReporteOdontogramas.aspx.designer.cs
+++ SAC/SAC/formularios/frm_ReporteOdontogramas.aspx.designer.cs
@@ -31,42 +31,6 @@ namespace SAC.formularios {
         protected global::System.Web.UI.ScriptManager sm;
         
         /// <summary>
-        /// Control colorO.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HiddenField colorO;
-        
-        /// <summary>
-        /// Control dienteO.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HiddenField dienteO;
-        
-        /// <summary>
-        /// Control seccionO.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HiddenField seccionO;
-        
-        /// <summary>
-        /// Control contextoO.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HiddenField contextoO;
-        
-        /// <summary>
         /// Control GridView_reporteOdontograma.
         /// </summary>
         /// <remarks>
